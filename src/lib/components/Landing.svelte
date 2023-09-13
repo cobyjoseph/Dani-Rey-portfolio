36de38b9bcf03b45a8005a1de59e1b7919f9f2c6
Coby Joseph <coby.joseph@gmail.com>
2023-09-13 18:21:34 -0400

    using compressed version

--- src/lib/components/Landing.svelte
+++ src/lib/components/Landing.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import landingGif from '$lib/assets/LandingGif.gif';
+	import landingGif from '$lib/assets/LandingGifCompressed.gif';
 </script>
-
 
 <img src={landingGif} alt="" class="w-full" />
