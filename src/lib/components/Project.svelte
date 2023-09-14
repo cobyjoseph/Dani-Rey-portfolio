eab5ff432781871a125a14d36c79d5e8772a02cf
Coby Joseph <coby.joseph@gmail.com>
2023-09-14 05:46:54 -0400

    tagging closer

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -8,104 +8,111 @@
 
 {#each projectsData as i}
 	<div class="grid grid-cols-2 border border-primaryDark">
-		<div class="flex flex-col">
-			<!-- header -->
+		<div class="flex flex-col p-7 justify-between min-h-[40rem]">
 			<div>
-				{$currentLanguage === 'En' ? i.titleEn : i.titleSp}
+				<!-- header -->
+
+				<div class="text-2xl font-genSans font-medium">
+					{$currentLanguage === 'En' ? i.titleEn : i.titleSp}
+				</div>
 
 				<!-- tags -->
-				<div>
-					{#if $currentLanguage === 'En'}
-						{#if i.brandIdentity === true}
-							Brand itentity
-						{/if}
+				<div class="flex gap-3 text-primary text-xs font-bold font-genSans">
+					<div class="bg-primaryDark py-[0.875rem] px-[1.125rem] rounded-[1.875rem]">
+						{#if $currentLanguage === 'En'}
+							{#if i.brandIdentity === true}
+								Brand itentity
+							{/if}
 
-						{#if i.brandDesignKit === true}
-							Brand Design Kit
-						{/if}
+							{#if i.brandDesignKit === true}
+								Brand Design Kit
+							{/if}
 
-						{#if i.webDesign === true}
-							Web Design
-						{/if}
+							{#if i.webDesign === true}
+								Web Design
+							{/if}
 
-						{#if i.stationaryDesign === true}
-							Stationary Design
-						{/if}
+							{#if i.stationaryDesign === true}
+								Stationary Design
+							{/if}
 
-						{#if i.socialMediaDesign === true}
-							Social Media Design
-						{/if}
+							{#if i.socialMediaDesign === true}
+								Social Media Design
+							{/if}
 
-						{#if i.strategy === true}
-							Strategy
-						{/if}
+							{#if i.strategy === true}
+								Strategy
+							{/if}
 
-						{#if i.packagingDesign === true}
-							Packaging Design
-						{/if}
+							{#if i.packagingDesign === true}
+								Packaging Design
+							{/if}
 
-						{#if i.productDesign === true}
-							Product Design
-						{/if}
+							{#if i.productDesign === true}
+								Product Design
+							{/if}
 
-						{#if i.uxUi === true}
-							UX/UI
-						{/if}
+							{#if i.uxUi === true}
+								UX/UI
+							{/if}
 
-						{#if i.cobranding === true}
-							Cobranding
-						{/if}
-					{:else}
-						{#if i.identidadMarca === true}
-							Identidad de Marca
-						{/if}
+							{#if i.cobranding === true}
+								Cobranding
+							{/if}
+						{:else}
+							{#if i.identidadMarca === true}
+								Identidad de Marca
+							{/if}
 
-						{#if i.kitIdentidad === true}
-							Kit de Identidad de Marca
-						{/if}
+							{#if i.kitIdentidad === true}
+								Kit de Identidad de Marca
+							{/if}
 
-						{#if i.disenoWeb === true}
-							Diseño Web
-						{/if}
+							{#if i.disenoWeb === true}
+								Diseño Web
+							{/if}
 
-						{#if i.disenoPapeleria === true}
-							Diseño de Papelería
-						{/if}
+							{#if i.disenoPapeleria === true}
+								Diseño de Papelería
+							{/if}
 
-						{#if i.disenoRedes === true}
-							Diseño de Redes Sociales
-						{/if}
+							{#if i.disenoRedes === true}
+								Diseño de Redes Sociales
+							{/if}
 
-						{#if i.estrategia === true}
-							Estrategia
-						{/if}
+							{#if i.estrategia === true}
+								Estrategia
+							{/if}
 
-						{#if i.disenoPackaging === true}
-							Diseño de Packaging
-						{/if}
+							{#if i.disenoPackaging === true}
+								Diseño de Packaging
+							{/if}
 
-						{#if i.disenoProducto === true}
-							Diseño de Producto
-						{/if}
+							{#if i.disenoProducto === true}
+								Diseño de Producto
+							{/if}
 
-						{#if i.disenoUxUi === true}
-							Diseño UX/UI
-						{/if}
+							{#if i.disenoUxUi === true}
+								Diseño UX/UI
+							{/if}
 
-						{#if i.cobranding === true}
-							Cobranding
+							{#if i.cobranding === true}
+								Cobranding
+							{/if}
 						{/if}
-					{/if}
+					</div>
 				</div>
 			</div>
 
-			<!-- description -->
 			<div>
-				{$currentLanguage === 'En' ? i.descriptionEn : i.descriptionSp}
-			</div>
-			<!-- link -->
-			<div>
-				{$currentLanguage === 'En' ? i.linkTitleEn : i.linkTitleSp}
+				<!-- description -->
+				<div>
+					{$currentLanguage === 'En' ? i.descriptionEn : i.descriptionSp}
+				</div>
+				<!-- link -->
+				<div>
+					{$currentLanguage === 'En' ? i.linkTitleEn : i.linkTitleSp}
+				</div>
 			</div>
 		</div>
 		<div>
