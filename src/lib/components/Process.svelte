875c4fa7046f8b3d11c874980638e56baf446c88
Coby Joseph <coby.joseph@gmail.com>
2023-09-20 16:40:24 -0400

    process diagram working

--- src/lib/components/Process.svelte
+++ src/lib/components/Process.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-col text-primary mx-[5.5%] gap-4">
+<div class="flex flex-col text-primary mx-[5.5%]">
 	<!-- title of the section -->
 	<div class="flex justify-between">
 		<!-- top row of boxes -->
@@ -13,96 +13,122 @@
 		</div>
 	</div>
 
+	<div class="flex justify-center h-[100px] items-center">
+		<div class="w-[83%] border-t-[2px] border-dashed border-primary" />
+	</div>
+
 	<div class="flex justify-between">
 		<!-- top row of boxes -->
 		<div class="w-[18%] invisible" />
-		<div class="w-[18%] border border-primary">box with lots of text example blah blah</div>
+		<div class="w-[18%] border border-primary relative pseudoCircleBefore1">
+			box with lots of text example blah blah
+		</div>
 		<div class="w-[18%] invisible border border-primary" />
 
-		<div class="w-[18%] border border-primary">box with lots of text example blah blah</div>
+		<div class="w-[18%] border border-primary relative pseudoCircleBefore2">
+			box with lots of text example blah blah
+		</div>
 		<div class="w-[18%] invisible border border-primary" />
 	</div>
 </div>
-
-<!-- <div class="flex justify-between mx-[6%]">
-      <div class="p-5 w-5 h-5 justify-center items-center border rounded-full">1</div>
-      <div class="p-5 w-5 h-5 justify-center items-center border rounded-full">2</div>
-      <div class="p-5 w-5 h-5 justify-center items-center border rounded-full">3</div>
-      <div class="p-5 w-5 h-5 justify-center items-center border rounded-full">4</div>
-      <div class="p-5 w-5 h-5 justify-center items-center border rounded-full">5</div>
-</div> -->
-
-<!-- <div class="grid grid-cols-6 row gap-[25px] bg-red-500">
-	<div class=" col-span-2 outline border border-primary">
-		<div class=" mx-auto border border-primary flex w-20">stuff in here</div>
-	</div>
-	<div class="col-span-2 border border-primary">
-		<div class=" mx-auto border border-primary flex w-20">stuff in here</div>
-	</div>
-	<div class=" col-span-2 border border-primary">
-		<div class=" mx-auto border border-primary flex w-20">stuff in here</div>
-	</div>
-
-
-</div>
-
-<div class="line-container outline">
-	<div class="line" />
-</div>
-
-<div class="grid grid-cols-6 row gap-[25px] bg-red-500">
-	<div class=" col-span-3 outline border border-primary">
-		<div class=" mx-auto border border-primary flex w-20">stuff in here</div>
-	</div>
-	<div class="col-span-3 border border-primary">
-		<div class=" mx-auto border border-primary flex w-20">stuff in here</div>
-	</div>
-
-</div> -->
 
 <style>
 	.pseudoCircleAfter1::after {
 		content: '1'; /* The number you want to display */
 		position: absolute;
-		bottom: -30px; /* Adjust as needed */
+		bottom: -80px; /* Adjust as needed */
 		left: 50%;
 		transform: translateX(-50%);
-		width: 24px; /* Width of the circle */
-		height: 24px; /* Height of the circle */
-		background-color: #ff4500; /* Color of the circle */
+		width: 60px; /* Width of the circle */
+		height: 60px; /* Height of the circle */
+		background-color: #333333; /* Color of the circle */
 		color: white; /* Color of the number */
 		text-align: center;
-		line-height: 24px; /* Aligns the text vertically */
+		line-height: 54px; /* Aligns the text vertically */
+		border: 2px solid #fffbde;
 		border-radius: 50%; /* Makes it a circle */
+		color: #fffbde;
+		font-family: General Sans;
+		font-size: 30px;
 	}
 
 	.pseudoCircleAfter2::after {
-		content: '2'; /* The number you want to display */
+		content: '3'; /* The number you want to display */
 		position: absolute;
-		bottom: -30px; /* Adjust as needed */
+		bottom: -80px; /* Adjust as needed */
 		left: 50%;
 		transform: translateX(-50%);
-		width: 24px; /* Width of the circle */
-		height: 24px; /* Height of the circle */
-		background-color: #ff4500; /* Color of the circle */
+		width: 60px; /* Width of the circle */
+		height: 60px; /* Height of the circle */
+		background-color: #333333; /* Color of the circle */
 		color: white; /* Color of the number */
 		text-align: center;
-		line-height: 24px; /* Aligns the text vertically */
+		line-height: 54px; /* Aligns the text vertically */
+		border: 2px solid #fffbde;
+
 		border-radius: 50%; /* Makes it a circle */
+		color: #fffbde;
+		font-family: General Sans;
+		font-size: 30px;
 	}
 
 	.pseudoCircleAfter3::after {
-		content: '3'; /* The number you want to display */
+		content: '5'; /* The number you want to display */
 		position: absolute;
-		bottom: -30px; /* Adjust as needed */
+		bottom: -80px; /* Adjust as needed */
 		left: 50%;
 		transform: translateX(-50%);
-		width: 24px; /* Width of the circle */
-		height: 24px; /* Height of the circle */
-		background-color: #ff4500; /* Color of the circle */
+		width: 60px; /* Width of the circle */
+		height: 60px; /* Height of the circle */
+		background-color: #333333; /* Color of the circle */
 		color: white; /* Color of the number */
 		text-align: center;
-		line-height: 24px; /* Aligns the text vertically */
+		line-height: 54px; /* Aligns the text vertically */
+		border: 2px solid #fffbde;
+
 		border-radius: 50%; /* Makes it a circle */
+		color: #fffbde;
+		font-family: General Sans;
+		font-size: 30px;
+	}
+
+	.pseudoCircleBefore1::before {
+		content: '2'; /* The number you want to display */
+		position: absolute;
+		top: -80px; /* Adjust as needed */
+		left: 50%;
+		transform: translateX(-50%);
+		width: 60px; /* Width of the circle */
+		height: 60px; /* Height of the circle */
+		background-color: #333333; /* Color of the circle */
+		color: white; /* Color of the number */
+		text-align: center;
+		line-height: 54px; /* Aligns the text vertically */
+		border: 2px solid #fffbde;
+
+		border-radius: 50%; /* Makes it a circle */
+		color: #fffbde;
+		font-family: General Sans;
+		font-size: 30px;
+	}
+
+	.pseudoCircleBefore2::before {
+		content: '4'; /* The number you want to display */
+		position: absolute;
+		top: -80px; /* Adjust as needed */
+		left: 50%;
+		transform: translateX(-50%);
+		width: 60px; /* Width of the circle */
+		height: 60px; /* Height of the circle */
+		background-color: #333333; /* Color of the circle */
+		color: white; /* Color of the number */
+		text-align: center;
+		line-height: 54px; /* Aligns the text vertically */
+		border: 2px solid #fffbde;
+
+		border-radius: 50%; /* Makes it a circle */
+		color: #fffbde;
+		font-family: General Sans;
+		font-size: 30px;
 	}
 </style>
