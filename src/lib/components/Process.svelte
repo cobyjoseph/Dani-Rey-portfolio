f1cfc28bfd62243fe5be3e664c7bfb9bfb20f2a3
Coby Joseph <coby.joseph@gmail.com>
2023-09-20 15:48:48 -0400

    before getting rid of grid systemforprocessdiagram

--- src/lib/components/Process.svelte
+++ src/lib/components/Process.svelte
@@ -18,3 +18,22 @@
 	<!-- numbers -->
 	<!-- bottom row of boxes -->
 </div>
+
+<div class="line-container outline">
+	<div class="line" />
+</div>
+
+<div class="grid grid-cols-6 row gap-[25px] bg-red-500">
+	<div class=" col-span-3 outline border border-primary">
+		<!-- box 1 -->
+		<div class=" mx-auto border border-primary flex w-20">stuff in here</div>
+	</div>
+	<div class="col-span-3 border border-primary">
+		<!-- box 2 -->
+		<div class=" mx-auto border border-primary flex w-20">stuff in here</div>
+	</div>
+
+	<!-- top row of boxes -->
+	<!-- numbers -->
+	<!-- bottom row of boxes -->
+</div>
